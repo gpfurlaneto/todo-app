f71b0204dfd4de2bb338131bbf6205b5d913e5e0
gpfurlaneto <gpfurlaneto@gmail.com>
2017-07-26 23:39:28 -0300

    início

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Tarefas' small='Cadastro'/>
+
+        <h2>Nossa histórioa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
---
+++ frontend/src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Provider from 'react-redux'
+
+import App from './main/app'
+import reducers from './main/reducers'
+
+const store = createStore(reducers)
+
+ReactDOM.render(
+    <Provider >
+        <App />
+    </Provider>
+, document.getElementById('app'))
